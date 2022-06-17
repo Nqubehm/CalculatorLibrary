1af440b126d88062ba534459bca9f31cc470260f
Lungile Mngonyama <gamedzenonny@gmail.com>
2022-06-17 22:16:29 +0200

    Added addition and subtraction functions

---
+++ calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+"""
+Calculator library containing basic math operations
+"""
+
+def add(a, b):
+    return a + b
+
+def substract(a, b):
+    return a - b
+
+
+# In[ ]:
+
+
+
+
